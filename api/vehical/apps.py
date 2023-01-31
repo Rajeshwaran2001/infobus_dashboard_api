85fb193c32f80c97f3d878ac198a0e3f05b1dc24
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-01-31 13:59:08 +0530

    vehical app basic setup updated

--- api/vehical/apps.py
+++ api/vehical/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class VehicalConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'vehical'
+    name = 'api.vehical'
--- api/vehical/models.py
+++ api/vehical/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from api.District.models import District
 # Create your models here.
+
+
+class Vehicle(models.Model):
+    RouteName = models.ForeignKey(District, on_delete=models.CASCADE)
+    VehicleNumer = models.TextField(max_length=6, null=True, blank=False)
