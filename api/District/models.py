a27cc516ee7e2bf6b03d8373315a27d5273d5dd2
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-01-31 13:59:38 +0530

    district app basic setup done

--- api/District/models.py
+++ api/District/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class District(models.Model):
+    District = models.CharField(max_length=20, null=True, blank=True)
+    DistrictTamil = models.CharField(max_length=50, null=True, blank=True)
