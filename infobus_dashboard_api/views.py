533b1868c58fb5183a3034910598ac17c86c9814
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-02-20 00:30:50 +0530

    customer view updated

--- infobus_dashboard_api/views.py
+++ infobus_dashboard_api/views.py
@@ -1,3 +1,4 @@
+import requests
 from django.shortcuts import render, redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -5,7 +6,9 @@ from django.db.models import Sum
 from api.ads.models import Ads
 from dashboard.models import MyAds
 from utility.models import bus_Detail
-
+from django.utils import timezone
+from datetime import timedelta, date
+import json
 
 # Create your views here.
 
@@ -64,8 +67,17 @@ def customer_view(request):
         bus_nos = bus_Detail.objects.filter(imei__in=myad).values_list('bus_no', 'route_no').distinct()
         ad.myads_count = MyAds.objects.filter(adname=ad.AdName).aggregate(Sum('Count'))['Count__sum']
         ad.myads_count = ad.myads_count if ad.myads_count is not None else 0  # To Print the total count is 0
+        day = timezone.now().date() - timedelta(days=1)
+        today = date.today()
+        yesterday = day.strftime("%#d/%#m/%Y")
+        total_count_yesterday = MyAds.objects.filter(adname=ad.AdName, date_time__contains=yesterday).aggregate(Sum(
+            'Count'))[
+                                    'Count__sum'] or 0
+        if not total_count_yesterday:
+            total_count_yesterday = 0
         # print(myad)
         # print(bus_nos)
+        # print(total_count_yesterday, yesterday)
 
         if ad.myads_count is not None:  # To handle the total count is 0
             if ad.TotalCount:
@@ -77,7 +89,37 @@ def customer_view(request):
             ad.percentage = 0
 
         mylist = zip(myad, bus_nos)
-        return render(request, 'customer/detail.html', {'ad': ad, 'mylist': mylist})
+        # Fetch API data and give inital data
+        params = {
+            'name': ad.AdName,
+            'from': today.strftime("%Y-%m-%d"),
+            'length': 1,
+        }
+        url1 = 'https://delta.busads.in/get_adcountv2.php'
+        api_data = requests.get(url1, params=params).json()
+        url2 = 'https://track.siliconharvest.net/get_adcountv2.php'
+        api_data2 = requests.get(url2, params=params).json()
+
+        # Print API responses to console
+        # print(api_data)
+        # print(api_data2)
+
+        # Add the two API responses
+        result = int(api_data) + int(api_data2)
+
+        # Create a dictionary with the result
+        data = result
+
+        # Convert dictionary to JSON
+        json_data = json.dumps(data)
+
+        context = {
+            'ad': ad,
+            'mylist': mylist,
+            'yesterday': total_count_yesterday,
+            'data': json_data
+        }
+        return render(request, 'customer/detail.html', context)
 
     # If request method is not POST, render the form
     return render(request, 'customer/login.html')
