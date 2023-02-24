9e58ae216f1e8ab062527387e8e64667314e6143
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-02-24 22:45:38 +0530

    customer view updated

--- infobus_dashboard_api/views.py
+++ infobus_dashboard_api/views.py
@@ -63,29 +63,26 @@ def customer_view(request):
             return redirect('customer-view')
 
         # rest of the view logic
-        myad = MyAds.objects.filter(adname=ad.AdName).values_list('imei', flat=True).distinct()
         ad = Ads.objects.get(AdName=ad_name_upper)
-        bus_nos = bus_Detail.objects.filter(imei__in=myad).values_list('bus_no', 'route_no').distinct()
+        ad.myads_count = MyAds.objects.filter(adname=ad.AdName).aggregate(Sum('Count'))['Count__sum']
+        ad.myads_count = ad.myads_count if ad.myads_count is not None else 0  # To Print the total count is 0
         day = timezone.now().date() - timedelta(days=1)
         today = date.today()
         yesterday = day.strftime("%#d/%#m/%Y")
-        total_count_yesterday = MyAds.objects.filter(adname=ad.AdName, date_time__contains=yesterday).aggregate(Sum(
-            'Count'))[
-                                    'Count__sum'] or 0
+        total_count_yesterday = \
+        MyAds.objects.filter(adname=ad.AdName, date_time__contains=yesterday).aggregate(Sum('Count'))['Count__sum'] or 0
         if not total_count_yesterday:
             total_count_yesterday = 0
         # print(myad)
         # print(bus_nos)
         # print(total_count_yesterday, yesterday)
 
-
-        mylist = zip(myad, bus_nos)
-        # Fetch API data and give inital data
+            # Fetch API data and give inital data
         params = {
-            'name': ad.AdName,
-            'from': today.strftime("%Y-%m-%d"),
-            'length': 1,
-        }
+                'name': ad.AdName,
+                'from': today.strftime("%Y-%m-%d"),
+                'length': 1,
+         }
         url1 = 'https://delta.busads.in/get_adcountv2.php'
         api_data = requests.get(url1, params=params).json()
         url2 = 'https://track.siliconharvest.net/get_adcountv2.php'
@@ -104,12 +101,49 @@ def customer_view(request):
         # Convert dictionary to JSON
         json_data = json.dumps(data)
 
-        context = {
-            'ad': ad,
-            'mylist': mylist,
-            'yesterday': total_count_yesterday,
-            'data': json_data
+        params2 = {
+                'name': ad.AdName,
+                'from': today.strftime("%Y-%m-%d"),
+                'length': 0,
         }
+
+        # fetch count data from API
+        url1 = 'https://track.siliconharvest.net/get_adcountv2.php'
+        response1 = requests.get(url1, params2)
+        try:
+             data1 = response1.json()
+        except ValueError:
+            data1 = []
+
+        url2 = 'https://delta.busads.in/get_adcountv2.php'
+        response2 = requests.get(url2, params2)
+        try:
+               data2 = response2.json()
+        except ValueError:
+             data2 = []
+
+        # Extract required information from data1 and data2
+        result = []
+        for data in [data1, data2]:
+            for item in data:
+                for key, value in item.items():
+                        if key != 'imei' and key != 'bus_no' and key != 'route_no' and key != 'route_name':
+                            d = {
+                                'imei': item['imei'],
+                                'bus_no': item['bus_no'],
+                                'route_no': item['route_no'],
+                                'route_name': item['route_name'],
+                                'date': key,
+                                'count': value,
+                            }
+                            result.append(d)
+
+            context = {
+                'ad': ad,
+                'result': result,
+                'yesterday': total_count_yesterday,
+                'data': json_data,
+            }
         return render(request, 'customer/detail.html', context)
 
     # If request method is not POST, render the form
