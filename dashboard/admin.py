3937d0347176a37b5a8f9b9c1cce8f473c825852
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-03-03 00:24:29 +0530

    Update admin.py

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MyAds, Franchise
+from .models import MyAds, Franchise, TaskResult
 # Register your models here.
 admin.site.register(MyAds)
 admin.site.register(Franchise)
+admin.site.register(TaskResult)
