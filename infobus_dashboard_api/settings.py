24c92cdb905c16a5493adec201b56ca1a0d9d34d
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-03-09 18:31:12 +0530

    settings env configured

--- infobus_dashboard_api/settings.py
+++ infobus_dashboard_api/settings.py
@@ -13,15 +13,21 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from datetime import timedelta
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-no_!_a-*0&a044^zlz@4*bcz@c&%xv-*3204hjw65r2g-1%091'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -81,8 +87,12 @@ WSGI_APPLICATION = 'infobus_dashboard_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASS'),
+        'HOST': env('DATABASE_HOST'),   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
