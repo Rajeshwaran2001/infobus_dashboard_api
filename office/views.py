d9027cff8ce3e86dc6a049b4cf36befcb69a454e
Rajeshwaran <54212165+Rajeshwaran2001@users.noreply.github.com>
2023-03-14 00:14:43 +0530

    display ads based on choice

--- office/views.py
+++ office/views.py
@@ -16,53 +16,99 @@ def is_office(user):
 @user_passes_test(is_office)
 def dashboard(request):
     district = District.objects.all().filter(is_Active=True)
-    ads = Ads.objects.all()
+    dist = request.GET.get('district')
+    district_data = None
+    if dist and dist.lower() != 'all':
+        ads = Ads.objects.all().filter(District=dist)
+        ten_days = []
+        five_days = []
+        for ad in ads:
+            if ad.diff <= 10 and ad.diff >= 5:
+                ten_days.append(ad)
+                # print(ten_days)
+            elif ad.diff <= 5:
+                five_days.append(ad)
+                # print(five_days)
 
-    ten_days = []
-    five_days = []
-    for ad in ads:
-        if ad.diff <= 10 and ad.diff >= 5:
-            ten_days.append(ad)
-            # print(ten_days)
-        elif ad.diff <= 5:
-            five_days.append(ad)
-            # print(five_days)
+            ad.myads_count = \
+                MyAds.objects.filter(adname=ad.AdName, date_time__range=[ad.StartDate, ad.EndDate]).aggregate(
+                    Sum('Count'))[
+                    'Count__sum']
 
-        ad.myads_count = \
-            MyAds.objects.filter(adname=ad.AdName, date_time__range=[ad.StartDate, ad.EndDate]).aggregate(Sum('Count'))[
-                'Count__sum']
+            ad.myads_count = ad.myads_count if ad.myads_count is not None else 0  # To Print the total count is 0
+            statuss = ad.day * ad.ECPD
+            diff = abs(ad.myads_count - statuss)
+            if ad.myads_count == 0:
+                ad.status = "crtical"
+            elif diff == 0:
+                ad.status = "up"
+            elif diff <= 200:
+                ad.status = "up"
+            elif diff > 200 and diff <= 400:
+                ad.status = "down"
+            elif ad.myads_count > statuss:
+                ad.status = "up"
+            else:
+                ad.status = "error"
 
-        ad.myads_count = ad.myads_count if ad.myads_count is not None else 0  # To Print the total count is 0
-        statuss = ad.day * ad.ECPD
-        diff = abs(ad.myads_count - statuss)
-        if ad.myads_count == 0:
-            ad.status = "crtical"
-        elif diff == 0:
-            ad.status = "up"
-        elif diff <= 200:
-            ad.status = "up"
-        elif diff > 200 and diff <= 400:
-            ad.status = "down"
-        elif ad.myads_count > statuss:
-            ad.status = "up"
-        else:
-            ad.status = "error"
-
-        # print(ad.day, ad.ECPD, statuss, ad.status, diff)
-        if ad.myads_count is not None:  # To handle the total count is 0
-            if ad.TotalCount:
-                # print(ad.AdName, ad.myads_count, ad.TotalCount)
-                ad.percentage = (ad.myads_count / ad.TotalCount) * 100
+            # print(ad.day, ad.ECPD, statuss, ad.status, diff)
+            if ad.myads_count is not None:  # To handle the total count is 0
+                if ad.TotalCount:
+                    # print(ad.AdName, ad.myads_count, ad.TotalCount)
+                    ad.percentage = (ad.myads_count / ad.TotalCount) * 100
+                else:
+                    ad.percentage = 0
             else:
                 ad.percentage = 0
-        else:
-            ad.percentage = 0
+    else:
+        ads = Ads.objects.all()
+        ten_days = []
+        five_days = []
+        for ad in ads:
+            if ad.diff <= 10 and ad.diff >= 5:
+                ten_days.append(ad)
+                # print(ten_days)
+            elif ad.diff <= 5:
+                five_days.append(ad)
+                # print(five_days)
+
+            ad.myads_count = \
+            MyAds.objects.filter(adname=ad.AdName, date_time__range=[ad.StartDate, ad.EndDate]).aggregate(
+                Sum('Count'))[
+                'Count__sum']
+
+            ad.myads_count = ad.myads_count if ad.myads_count is not None else 0  # To Print the total count is 0
+            statuss = ad.day * ad.ECPD
+            diff = abs(ad.myads_count - statuss)
+            if ad.myads_count == 0:
+                ad.status = "crtical"
+            elif diff == 0:
+                ad.status = "up"
+            elif diff <= 200:
+                ad.status = "up"
+            elif diff > 200 and diff <= 400:
+                ad.status = "down"
+            elif ad.myads_count > statuss:
+                ad.status = "up"
+            else:
+                ad.status = "error"
+
+                # print(ad.day, ad.ECPD, statuss, ad.status, diff)
+            if ad.myads_count is not None:  # To handle the total count is 0
+                if ad.TotalCount:
+                    # print(ad.AdName, ad.myads_count, ad.TotalCount)
+                    ad.percentage = (ad.myads_count / ad.TotalCount) * 100
+                else:
+                    ad.percentage = 0
+            else:
+                ad.percentage = 0
 
     context = {
         'district': district,
         'ads': ads,
         'ten_days': ten_days,
         'five_days': five_days,
+        'selected_district': dist
     }
     return render(request, 'office/dashboard.html', context)
 
